9d8e7229e8e12493a2e20217ac0396a4b1769e2e
IharKhakholka <igor.xoxolko@gmail.com>
2018-12-08 23:43:26 +0300

    com

---
+++ bot.py
@@ -0,0 +1,40 @@
+from telethon import TelegramClient, sync
+from telethon import functions, types
+from telethon import utils
+from telethon import events
+
+api_id = 388006                  # API ID (получается при регистрации приложения на my.telegram.org)
+api_hash = "90d6e13ca33ac535ef7002a698d7258c"              # API Hash (оттуда же)
+phone_number = "+375259441173"    # Номер телефона аккаунта, с которого будет выполняться код
+
+client = TelegramClient('SomeName', api_id, api_hash).start()
+
+
+username = '@ihoholko'
+mak = '@nmakeychik'
+me = client.get_me()
+from telethon import utils
+from telethon import events
+
+
+# 
+@client.on(events.NewMessage(incoming=True, chats=('+375 29 594 8978', 'Приватный канал')))
+async def handler(event):
+	persone = event.client
+
+	dialogs = await persone.get_dialogs()
+	message = dialogs[0].message.message
+	# # await event.respond()
+
+	await client.send_message(mak, str(message)  )
+
+
+
+
+	# result = await client(functions.messages.DeleteHistoryRequest(
+	#    	peer=mak,
+	#     max_id=0
+	# ))
+
+print("I'm working")
+client.run_until_disconnected()
